9efd44db91bf3384151ddb5a1c5bf4c028d6203e
김건희 <tahm_kench@naver.com>
2022-07-14 14:52:40 +0900

    [Update] 1890 점프

--- Baekjoon/DP/1890_점프.cpp
+++ Baekjoon/DP/1890_점프.cpp
@@ -5,70 +5,34 @@
 
 using namespace std;
 
-int N;
-
-vector<vector<int>> dataArr(100, vector<int>(100, -1));
-
-int topDown(int x, int y, vector<vector<int>> arr) {
-
-	int tempIndex = arr[x][y];
-
-	if (x == N - 1 && y == N - 1) {
-		dataArr[x][y] = 0;
-
-		if (N > 9) {
-			for (int i = 1; i < 10; i++) {
-				if (i == arr[N - i - 1][N - 1]) {
-					dataArr[x][y] += topDown(x - i, y, arr);
-				}
-				if (i == arr[N - 1][N - i - 1]) {
-					dataArr[x][y] += topDown(x, y - i, arr);
-				}
-			}
-		}
-		else {
-			for (int i = 1; i < N - 1; i++) {
-				if (i == arr[N - i - 1][N - 1]) {
-					dataArr[x][y] += topDown(x - i, y, arr);
-				}
-				if (i == arr[N - 1][N - i - 1]) {
-					dataArr[x][y] += topDown(x, y - i, arr);
-				}
-			}
-		}
-	}
-	
-	if (dataArr[x][y] == -1) {
-		dataArr[x][y] = 0;
-
-		if (x - arr[x][y] >= 0) {
-			dataArr[x][y] += topDown(x - tempIndex, y, arr);
-		}
-		if (y - arr[x][y] >= 0) {
-			dataArr[x][y] += topDown(x, y - tempIndex, arr);
-		}
-	}
-
-	return dataArr[x][y];
-}
-
 int main() {
-	
+
+	int N;
 	cin >> N;
 
 	vector<vector<int>> arr(N, vector<int> (N));
-	
+	vector<vector<long long int>> numberArr(N, vector<long long int>(N));
+
+	numberArr[0][0] = 1;
+
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
 			cin >> arr[i][j];
 		}
 	}
 
-	dataArr[arr[0][0]][0] = 1;
-	dataArr[0][arr[0][0]] = 1;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (arr[i][j] != 0) {
+				if (i + arr[i][j] < N)
+					numberArr[i + arr[i][j]][j] += numberArr[i][j];
+				if (j + arr[i][j] < N)
+					numberArr[i][j + arr[i][j]] += numberArr[i][j];
+			}
+		}
+	}
 
-	// TopDown
-	cout << topDown(N - 1, N - 1, arr);
-
+	cout << numberArr[N - 1][N - 1];
+	
 	return 0;
 }
