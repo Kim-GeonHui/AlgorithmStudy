ffd6aa5541f6b640cc1493867c95d91fcbeafb05
김건희 <tahm_kench@naver.com>
2022-07-19 17:12:16 +0900

    [Update] 2294 동전 2

--- Baekjoon/DP/2294_동전 2.cpp
+++ Baekjoon/DP/2294_동전 2.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <cmath>
 #pragma warning (disable:4996)
 
 using namespace std;
@@ -11,47 +11,24 @@ int main() {
 	int N, K;
 	cin >> N >> K;
 
-	vector<int> arr(N);
+	vector<int> coinArr(N);
 	for (int i = 0; i < N; i++) {
-		cin >> arr[i];
+		cin >> coinArr[i];
 	}
 
-	sort(arr.begin(), arr.end());
-
-	vector<int> numberArr(K);
+	vector<int> resultArr(K + 1, 10001);
+	resultArr[0] = 0;
+	
 	for (int i = 0; i < N; i++) {
-		if (arr[i] - 1 < K)
-			numberArr[arr[i] - 1] = 1;
-	}
-
-	int tempIndex = 0;
-	int tempMin;
-
-	for (int i = arr[0]; i < K; i++) {
-		if (numberArr[i] != 0)
-			tempIndex++;
-		else {
-			tempMin = 0;
-			for (int j = 0; j < tempIndex + 1; j++) {
-				if (numberArr[i - arr[j]] != 0) {
-					tempMin = 1 + numberArr[i - arr[j]];
-					if (numberArr[i] == 0) {
-						numberArr[i] = tempMin;
-					}
-					else {
-						numberArr[i] = min(tempMin, numberArr[i]);
-					}
-				}
-			}
+		for (int j = coinArr[i]; j <= K; j++) {
+			resultArr[j] = min(resultArr[j], resultArr[j - coinArr[i]] + 1);
 		}
 	}
-	
-	if (numberArr[K - 1] == 0) {
+
+	if (resultArr[K] == 10001)
 		cout << -1;
-	}
-	else {
-		cout << numberArr[K - 1];
-	}
-	
+	else
+		cout << resultArr[K];
+
 	return 0;
 }
