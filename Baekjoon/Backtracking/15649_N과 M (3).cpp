ef6a40b4f7355bd1fc11a1f57d14123b0b2faf6d
김건희 <tahm_kench@naver.com>
2022-08-04 10:18:34 +0900

    [Add] 15649  N과 M (3)

---
+++ Baekjoon/Backtracking/15649_N과 M (3).cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <iostream>
+#pragma warning (disable:4996)
+
+using namespace std;
+
+int resultArr[8];
+
+int N, M;
+
+void printArr(int arr[]) {
+	for (int i = 0; i < M; i++) {
+		cout << arr[i] << ' ';
+	}
+	cout << endl;
+}
+
+void DFS(int count) {
+	if (count == M) {
+		printArr(resultArr);
+		return;
+	}
+
+	for (int i = 1; i < N + 1; i++) {
+		resultArr[count] = i;
+		DFS(count + 1);
+	}
+}
+
+int main() {
+
+	cin >> N >> M;
+	DFS(0);
+
+	return 0;
+}
