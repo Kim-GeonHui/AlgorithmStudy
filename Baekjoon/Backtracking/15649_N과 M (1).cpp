0137a25082088760473572553df655661e387495
김건희 <tahm_kench@naver.com>
2022-08-06 16:17:50 +0900

    [Update] 15649 N과 M(1)

--- Baekjoon/Backtracking/15649_N과 M (1).cpp
+++ Baekjoon/Backtracking/15649_N과 M (1).cpp
@@ -1,33 +1,27 @@
 #include <stdio.h>
 #include <iostream>
-#include <vector>
-#include <algorithm>
 #pragma warning (disable:4996)
 
 using namespace std;
 
-vector<bool> visitArr(9);
-vector<int> resultArr(9);
+bool visitArr[9];
+int resultArr[9];
 
 int N, M;
 
-void printArr(vector<int> arr) {
-	for (int i = 0; i < M; i++) {
-		cout << arr[i] << ' ';
-	}
-	cout << endl;
-}
-
 void DFS(int count) {
-	for (int i = 1; i < N + 1; i++) {
-		if (count == M) {
-			printArr(resultArr);
-			return;
+	if (count == M) {
+		for (int i = 0; i < M; i++) {
+			cout << resultArr[i] << ' ';
 		}
+		cout << '\n';
+		return;
+	}
 
+	for (int i = 1; i < N + 1; i++) {
 		if (!visitArr[i]) {
-			resultArr[count] = i;
 			visitArr[i] = true;
+			resultArr[count] = i;
 			DFS(count + 1);
 			visitArr[i] = false;
 		}
@@ -35,6 +29,9 @@ void DFS(int count) {
 }
 
 int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
 
 	cin >> N >> M;
 	DFS(0);
