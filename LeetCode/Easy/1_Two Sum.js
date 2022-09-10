635ca9c91af87a0fc48ccc68b3a952c748dbb334
김건희 <tahm_kench@naver.com>
2022-09-10 14:54:19 +0900

    [Update] 1 Two Sum

--- LeetCode/Easy/1_Two Sum.js
+++ LeetCode/Easy/1_Two Sum.js
@@ -5,7 +5,7 @@
  */
  var twoSum = function(nums, target) {
   for (let i = 0; i < nums.length; i++) {
-      if (nums.indexOf(target-nums[i])!==-1)
+      if (nums.indexOf(target-nums[i])!==-1 && nums.indexOf(target-nums[i])!==i)
           return [i, nums.indexOf(target-nums[i])];
   }
 };
