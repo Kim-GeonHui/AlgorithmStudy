6ddc326db313645c23d5c64edce04d79ae49233e
김건희 <tahm_kench@naver.com>
2022-09-10 16:45:32 +0900

    [Add] 66 Plus One

---
+++ LeetCode/Easy/66_Plus One.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+ var plusOne = function(digits) {
+  digits[digits.length-1] += 1;
+  
+  for (let i = digits.length - 1; i > 0; i--) {
+      if (digits[i] !== 10) break;
+      
+      digits[i] = 0;
+      digits[i - 1] += 1;
+  }
+  
+  if (digits[0] === 10) {
+      digits[0] = 0;
+      digits.unshift(1);
+  }
+  
+  return digits;
+};
