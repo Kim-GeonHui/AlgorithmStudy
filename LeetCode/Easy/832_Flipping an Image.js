452a3ec1c157c63e1dd9ded76610163c5825892d
김건희 <tahm_kench@naver.com>
2022-11-02 22:22:31 +0900

    [Add] 832 Flipping an Image

---
+++ LeetCode/Easy/832_Flipping an Image.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[][]} image
+ * @return {number[][]}
+ */
+ let flipAndInvertImage = function(image) {
+  
+  for (let i = 0; i < image.length; i++) {
+      image[i] = image[i].reverse();
+      for (let j = 0; j < image[i].length; j++) {
+          if (image[i][j] === 0) image[i][j] = 1;
+          else image[i][j] = 0;
+      }
+  }
+  
+  return image;
+};
