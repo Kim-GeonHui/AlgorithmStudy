8097f3ebe441584f29759cf6de3249d243ded84e
김건희 <tahm_kench@naver.com>
2022-11-19 23:12:53 +0900

    [Update] 10866 덱

--- Baekjoon/DataStructure/18258_큐 2 (2).cpp
+++ Baekjoon/DataStructure/18258_큐 2 (2).cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <iostream>
+#include <vector>
 #pragma warning (disable:4996)
 
 using namespace std;
@@ -8,20 +9,23 @@ class Node {
 public:
 	int value;
 	Node* next = nullptr;
+	Node* front = nullptr;
 	Node() {};
 	~Node() {};
 };
 
-class Stack {
+class Deque {
 public:
 	bool empty();
-	void push(int target);
-	int pop();
+	void push_front(int target);
+	void push_back(int target);
+	int pop_front();
+	int pop_back();
 	int size();
 	int front();
 	int back();
-	Stack() { head = nullptr; tail = nullptr; length = 0; };
-	~Stack() { while (!empty()) { pop(); } };
+	Deque() { head = nullptr; tail = nullptr; length = 0; };
+	~Deque() { while (!empty()) { pop_front(); } };
 private:
 	Node* head;
 	Node* tail;
@@ -32,56 +36,62 @@ int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	Stack stack;
+	Deque deque;
 
 	int N;
 	cin >> N;
 
 	string str;
-	int num;
+	int target = 0;
 
 	for (int i = 0; i < N; i++) {
 		cin >> str;
 
-		if (str == "push") {
-			cin >> num;
-			stack.push(num);
+		if (str == "push_back") {
+			cin >> target;
+			deque.push_back(target);
 		}
-		else if (str == "front")
-			cout << stack.front() << '\n';
-		else if (str == "back")
-			cout << stack.back() << '\n';
-		else if (str == "pop")
-			cout << stack.pop() << '\n';
+		else if (str == "push_front") {
+			cin >> target;
+			deque.push_front(target);
+		}
+		else if (str == "pop_front")
+			cout << deque.pop_front() << '\n';
+		else if (str == "pop_back")
+			cout << deque.pop_back() << '\n';
 		else if (str == "size")
-			cout << stack.size() << '\n';
+			cout << deque.size() << '\n';
 		else if (str == "empty")
-			cout << stack.empty() << '\n';
+			cout << deque.empty() << '\n';
+		else if (str == "front")
+			cout << deque.front() << '\n';
+		else if (str == "back")
+			cout << deque.back() << '\n';
 	}
 
 	return 0;
 }
 
-bool Stack::empty() {
+bool Deque::empty() {
 	if (length == 0) return 1;
 	else return 0;
 }
 
-int Stack::size() {
+int Deque::size() {
 	return length;
 }
 
-int Stack::front() {
+int Deque::front() {
 	if (length == 0) return -1;
 	else return head->value;
 }
 
-int Stack::back() {
+int Deque::back() {
 	if (length == 0) return -1;
 	else return tail->value;
 }
 
-void Stack::push(int target) {
+void Deque::push_front(int target) {
 	Node* node = new Node;
 	node->value = target;
 
@@ -91,20 +101,52 @@ void Stack::push(int target) {
 		length++;
 		return;
 	}
-	else {
-		tail->next = node;
-		tail = tail->next;
-		length++;
-	}
+
+	node->next = head;
+	head->front = node;
+	head = node;
+	length++;
 }
 
-int Stack::pop() {
-	if (length == 0) return -1;
+void Deque::push_back(int target) {
+	Node* node = new Node;
+	node->value = target;
+
+	if (length == 0) {
+		head = node;
+		tail = node;
+		length++;
+		return;
+	}
+
+	tail->next = node;
+	node->front = tail;
+	tail = tail->next;
+	length++;
+}
+
+int Deque::pop_front() {
+	if (length == 0) return -1;	
 
 	int answer = head->value;
 	Node* node = head;
 	head = node->next;
+	node->next = nullptr;
 	delete node;
 	length--;
+	 
+	return answer;
+}
+
+int Deque::pop_back() {
+	if (length == 0) return -1;
+
+	int answer = tail->value;
+	Node* node = tail;
+	tail = node->front;
+	node = nullptr;
+	delete node;
+	length--;
+
 	return answer;
 }
