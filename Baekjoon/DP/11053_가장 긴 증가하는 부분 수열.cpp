6043b0349a4acee2862f3942da94acb2f7b31a01
김건희 <tahm_kench@naver.com>
2023-02-25 19:11:04 +0900

    [Update] 11053 가장 긴 증가하는 부분 수열

--- Baekjoon/DP/11053_가장 긴 증가하는 부분 수열.cpp
+++ Baekjoon/DP/11053_가장 긴 증가하는 부분 수열.cpp
@@ -1,59 +1,33 @@
 #include <stdio.h>
 #include <iostream>
-#include <vector>
-#include <cmath>
 #include <algorithm>
+#include <vector>
 #pragma warning (disable:4996)
 
 using namespace std;
 
 int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
 
 	int N;
 	cin >> N;
 
-	vector<int> numberArr(N);
-	for (int i = 0; i < N; i++) {
-		cin >> numberArr[i];
-	}
+	vector<int> arr(N + 1);
+	vector<int> ans(N + 1, 1);
 
-	vector<vector<int>> arr(N);
-	vector<vector<int>> maxNumberArr(N);
-	for (int i = 0; i < N; i++) {
-		arr[i] = vector<int>(pow(2, i + 1));
-		maxNumberArr[i] = vector<int>(pow(2, i + 1));
-	}
-
-	arr[0][0] = 0;
-	arr[0][1] = 1;
-	maxNumberArr[0][1] = numberArr[0];
-	
-	for (int i = 1; i < N; i++) {
-		for (int j = 0; j < pow(2, i + 1); j++) {
-			if (j % 2 == 0) {
-				arr[i][j] = arr[i - 1][j / 2];
-				maxNumberArr[i][j] = maxNumberArr[i - 1][j / 2];
-			}
-			else if (maxNumberArr[i - 1][j / 2] < numberArr[i]) {
-				arr[i][j] = arr[i - 1][j / 2] + 1;
-				maxNumberArr[i][j] = numberArr[i];
-			}
-			else {
-				arr[i][j] = arr[i - 1][j / 2];
-				maxNumberArr[i][j] = maxNumberArr[i - 1][j / 2];
+	for (int i = 1; i <= N; i++) cin >> arr[i];
+	int answer = 1;
+	for (int i = 2; i <= N; i++) {
+		for (int j = i - 1; j > 0; j--) {
+			if (arr[i] > arr[j]) {
+				ans[i] = max(ans[i], ans[j] + 1);
+				answer = max(answer, ans[i]);
 			}
 		}
 	}
 
-	int result = 0;
-	int tempMax = 0;
-	for (int i = 0; i < N; i++) {
-		tempMax = *max_element(arr[i].begin(), arr[i].end());
-		if (tempMax > result)
-			result = tempMax;
-	}
-
-	cout << tempMax;
+	cout << answer;
 
 	return 0;
 }
