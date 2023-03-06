a0a6c6e0ca77e0ace108778c0a8a6afbffe13986
김건희 <tahm_kench@naver.com>
2023-03-06 22:23:30 +0900

    [Update] 10816 숫자 카드 2

--- Baekjoon/Binary Search/10816_숫자 카드 2.cpp
+++ Baekjoon/Binary Search/10816_숫자 카드 2.cpp
@@ -7,27 +7,55 @@
 
 using namespace std;
 
+vector<int> arr;
+
+int lower_idx(int t) {
+	int front, end, mid;
+	front = 0;
+	end = arr.size();
+	mid = (front + end) / 2;
+
+	while (front < end) {
+		if (arr[mid] < t) front = mid + 1;
+		else end = mid;
+		mid = (front + end) / 2;
+	}
+
+	return front;
+}
+
+int upper_idx(int t) {
+	int front, end, mid;
+	front = 0;
+	end = arr.size();
+	mid = (front + end) / 2;
+
+	while (front < end) {
+		if (arr[mid] <= t) front = mid + 1;
+		else end = mid;
+		mid = (front + end) / 2;
+	}
+
+	return end;
+}
+
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	int N, M, num;
+	int N, M;
 	cin >> N;
-
-	map<int, int> m;
-	for (int i = 0; i < N; i++) {
-		cin >> num;
-		if (m.find(num) == m.end()) {
-			m[num] = 1;
-		}
-		else m[num]++;
-	}
+	arr.assign(N, 0);
+	for (int i = 0; i < N; i++) cin >> arr[i];
 
 	cin >> M;
-	for (int i = 0; i < M; i++) {
-		cin >> num;
-		if (m.find(num) == m.end()) cout << 0 << ' ';
-		else cout << m[num] << ' ';
+	vector<int> list(M);
+	for (int i = 0; i < M; i++) cin >> list[i];
+
+	sort(arr.begin(), arr.end());
+
+	for (int target : list) {
+		cout << upper_idx(target) - lower_idx(target) << ' ';
 	}
 
 	return 0;
